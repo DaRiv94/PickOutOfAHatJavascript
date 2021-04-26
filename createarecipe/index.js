c0915f55f0883d888805bae3328c43ed44df0408
Frankie Riviera <frankgriviera@outlook.com>
2021-04-26 15:24:40 -0400

    added error handling for if a recipe added already exists

--- createarecipe/index.js
+++ createarecipe/index.js
@@ -3,28 +3,50 @@
 //https://docs.microsoft.com/en-us/azure/azure-functions/functions-triggers-bindings?tabs=javascript
 //https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-storage-table-output?tabs=javascript
 
-module.exports = async function (context, req) {
+module.exports = async function (context, req, existingRecipeEntities) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     context.bindings.recipeEntities = [];
     // recipeEntities = [];
 
     if (req.body && req.body.food && req.body.url) {
-        new_recipe = {
-            PartitionKey: "recipes",
-            RowKey: `r_${req.body.food}`,
-            url: `${req.body.url}`
+
+        //Check for existing recipe
+        let exists = false
+        for(let i=0;i<existingRecipeEntities.length;i++){
+            context.log("existingRecipeEntities[i].RowKey: ",existingRecipeEntities[i].RowKey)
+            context.log("`r_${req.body.food}`: ",`r_${req.body.food}`)
+
+            if(existingRecipeEntities[i].RowKey == `r_${req.body.food}`){
+                exists = true
+                context.log('set exists:',exists);
+            }
         }
-
-        context.bindings.recipeEntities.push(new_recipe);
-
-        context.res = {
-            status: 200, /* Defaults to 200 */
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: { newRecipe: new_recipe }
-        };
+        if (exists){
+            context.res = {
+                status: 400, /* Defaults to 200 */
+                body: { error: "That food already exists, please send a different food" },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+        }else{
+            new_recipe = {
+                PartitionKey: "recipes",
+                RowKey: `r_${req.body.food}`,
+                url: `${req.body.url}`
+            }
+    
+            context.bindings.recipeEntities.push(new_recipe);
+    
+            context.res = {
+                status: 200, /* Defaults to 200 */
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: { newRecipe: new_recipe }
+            };
+        }
     } else {
         context.res = {
             status: 400, /* Defaults to 200 */
